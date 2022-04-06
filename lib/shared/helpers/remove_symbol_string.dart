f6a4780e2b6f34c370ef7b4f3127c2e2c39c5299
Muhammad Rizky Nur Fajrie <86530834+mrizkynurfajrie@users.noreply.github.com>
2022-04-06 21:35:45 +0800

    remove unused dep & shared files

--- lib/shared/helpers/remove_symbol_string.dart
+++
@@ -1,5 +0,0 @@
-removeSymbolFromString(String txt) {
-  return txt
-      .replaceAll(RegExp(r'[^\w\s]+'), '')
-      .replaceAll(RegExp(r"\s\b|\b\s"), "");
-}
--- lib/shared/helpers/separated.dart
+++
@@ -1,7 +0,0 @@
-String separated(value) {
-  String first = value.toString().substring(0, 4);
-  String second = value.toString().substring(4, 8);
-  String third = value.toString().substring(8, 12);
-  String forth = value.toString().substring(12, 16);
-  return "$first $second $third $forth";
-}
